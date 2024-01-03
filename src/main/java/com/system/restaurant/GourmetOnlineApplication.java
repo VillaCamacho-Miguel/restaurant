475d7ccb54848b81a08033f8da16e51c858c0cc9
VillaCamacho <villacamacho87@gmail.com>
2024-01-03 15:40:29 -0700

    project to push to github start

---
+++ src/main/java/com/system/restaurant/GourmetOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.system.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetOnlineApplication.class, args);
+	}
+
+}
